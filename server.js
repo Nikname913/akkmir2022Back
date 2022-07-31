63c39500e3255b380cace1d7759804c02578ba02
nik shipov <nik.shipov@gmail.com>
2022-07-31 15:47:02 +0500

    UPD: проработка маршрута на получение каталога 310722

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express       = require('express')
 const bodyParser    = require('body-parser')
 const xmlparser     = require('express-xml-bodyparser')
 const app           = express()
+const fs            = require('fs')
 const port          = 8000
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -11,7 +12,70 @@ app.use(xmlparser());
 
 app.post('/xmlhub', (req, res) => {
 
-  res.send(req.body)
+  const xmldata = req.body
+  let consoleAnswer = ''
+  let productsCount = 0
+
+  const jsonFile = './app/data/akkmirCatalogLocal.json'
+  const jsonFileRegions = './app/data/akkmirCatalogLocalRegions.json'
+  const jsonFileShops = './app/data/akkmirCatalogLocalShops.json'
+  const jsonFileCategories = './app/data/akkmirCatalogLocalCategories.json'
+  const jsonFileDirections = './app/data/akkmirCatalogLocalDirections.json'
+  const jsonFileProducts = './app/data/akkmirCatalogLocalProducts.json'
+
+  const dataFileName = './app/data/dataFileName.json'
+
+  const regions = xmldata.gpc.regions
+  const shops = xmldata.gpc.warehouses
+  const categories = xmldata.gpc.groups
+  const directions = xmldata.gpc.directions
+  const products = xmldata.gpc.products
+  const files = xmldata.gpc.files
+
+  fs.writeFile(jsonFile, JSON.stringify(xmldata), error => {
+    if (error) throw error
+  })
+
+  fs.writeFile(jsonFileRegions, JSON.stringify(regions), error => {
+    if (error) throw error
+  })
+
+  fs.writeFile(jsonFileShops, JSON.stringify(shops), error => {
+    if (error) throw error
+  })
+
+  fs.writeFile(jsonFileCategories, JSON.stringify(categories), error => {
+    if (error) throw error
+  })
+
+  fs.writeFile(jsonFileDirections, JSON.stringify(directions), error => {
+    if (error) throw error
+  })
+
+  // =================================
+  // =================================
+
+  fs.writeFile(dataFileName, JSON.stringify(files), error => {
+    if (error) throw error
+  })
+
+  
+  for ( key in xmldata.gpc ) {
+
+    consoleAnswer += `Название поля: ${key}. `
+
+  }
+
+  res.send(`success. dataType: ${typeof(xmldata)}. Список доступных полей: ${consoleAnswer}`)
+
+})
+
+app.post('/catalog', (req, res) => {
+
+  const jsonFile = './app/data/akkmirCatalogLocal.json'
+  const catalog = fs.readFileSync(jsonFile, 'utf8')
+
+  res.send(catalog)
 
 })
 
