7fe3407aed37bd66f39475710d5737b5655916cd
nik shipov <nik.shipov@gmail.com>
2022-08-01 13:27:11 +0500

    UPD: update 010822

--- app/routes/getProducts.js
+++ app/routes/getProducts.js
@@ -1,6 +1,6 @@
 const fs = require('fs'); module.exports = (app, db = null) => {
 
-  db === null && null; app.post('/products', (req = null, res) => {
+  db === null && null; app.get('/products', (req = null, res) => {
 
     const productsFile = fs.readFileSync('./app/data/akkmirCatalogLocalProducts.json', 'utf-8')
     res.send(productsFile)
